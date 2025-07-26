5366a890402f2f3694aa531a799b3d42dff925b1
Salahsr2005 <salah.serir@etu.univ-batna2.dz>
2025-07-26 14:12:18 +0200

    Update GuestConsultation.tsx

--- src/pages/GuestConsultation.tsx
+++ src/pages/GuestConsultation.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { GuestModeWrapper } from '@/components/layout/GuestModeWrapper';
-import Consultation from './Consultation';
+import { GuestModeWrapper } from "@/components/layout/GuestModeWrapper"
+import ProgramConsultationFlow from "@/components/consultation/ProgramConsultationFlow"
 
 export default function GuestConsultation() {
   return (
     <GuestModeWrapper>
-      <Consultation />
+      <ProgramConsultationFlow />
     </GuestModeWrapper>
-  );
+  )
 }
